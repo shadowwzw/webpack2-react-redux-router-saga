30704c7cd1a71d3906fe83743267cde9a552e50f
shadowwzw <miditab@yandex.ru>
2017-03-24 20:43:34 +0400

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,7 @@ module.exports = {
               loader: 'css-loader',
               options: {
                 modules: true,
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
               }
             },
           ],
