83605eac08ac8d1deaefad3601c8ea41baaed981
shadowwzw <miditab@yandex.ru>
2017-03-24 20:37:58 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import style from './App.css';
+
+console.log(style);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className={style.App}>
+        <div className={style['App-header']}>
           <h2>Welcome to React!</h2>
         </div>
-        <p className="App-intro">
+        <p className={style['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
