4da718f79e93106bb732f8bd9d1d4fe3e4160c7a
shadowwzw <miditab@yandex.ru>
2017-03-25 14:46:44 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import style from './App.css';
+import logo from 'file-loader!./logo.svg';
 
 console.log(style);
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className={style.App}>
         <div className={style['App-header']}>
+          <img src={logo} className={style['App-logo']} alt="logo" />
           <h2>Welcome to React!</h2>
         </div>
         <p className={style['App-intro']}>
