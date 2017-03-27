3d915c7cd656e11623a3800d1a0e71cd4c8aaa4b
Gavrilov Ruslan <miditab@yandex.ru>
2017-03-27 10:57:35 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,35 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "jsx": true,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react"
-  ],
-  "extends": "eslint:recommended",
-  "env": {
-    "node": true,
-    "mocha": true,
-    "es6": true,
-    "browser": true,
-  },
-  "rules": {
-    "prefer-const": 2,
-    "no-console": 0
-  },
-  "parser": "babel-eslint",
-  "globals": {
-    "production": true,
-    "development": true
-  },
-}
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "jsx": "true",
+        "sourceType": "module"
+    },
+    "ecmaFeatures": {
+        "jsx": true,
+        "modules": true
+    },
+    "plugins": [
+        "react",
+        "import"
+    ],
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true,
+        "mocha": true,
+        "es6": true,
+        "browser": true
+    },
+    "rules": {
+        "prefer-const": 2,
+        "no-console": 0,
+        "strict": [2, "never"],
+        "react/jsx-uses-react": 2,
+        "react/jsx-uses-vars": 2,
+        "react/react-in-jsx-scope": 2
+    },
+    "parser": "babel-eslint",
+    "globals": {
+        "production": true,
+        "development": true
+    }
+};
